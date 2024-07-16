79c9982a9e1d2bfb19cd1728c56e06bf8911a323
gimasteevroman <best.roma@gmail.com>
2024-07-16 18:39:06 +0500

    Урок 2 | Gps Tracker 1) подвязал меню к xml 2) добавил слушатель нажатий в main 3) для теста кнопок добавил тосты

--- app/src/main/java/com/galileosky/gsgpstracker/MainActivity.kt
+++ app/src/main/java/com/galileosky/gsgpstracker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.galileosky.gsgpstracker
 
 import android.os.Bundle
+import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
 import com.galileosky.gsgpstracker.databinding.ActivityMainBinding
@@ -11,5 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        onButtonNavClicks()
+    }
+
+    private fun onButtonNavClicks() {
+        binding.bNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.id_home -> Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                R.id.id_tracks -> Toast.makeText(this, "Tracks", Toast.LENGTH_SHORT).show()
+                R.id.id_settings -> Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
     }
 }

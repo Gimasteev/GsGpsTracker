8b08605b88ce7b4c7a9007837601b4b573863cbd
gimasteevroman <best.roma@gmail.com>
2024-08-21 18:36:03 +0500

    Задача №11 1) Добавить функицю отслеживания местоположения на карте 2) Добавил разрешения на точную и примерную геопозицию 3) Добавил провайдера для работы с GPS, отображение текущего местоположения на карте, слои

--- app/src/main/java/com/galileosky/gsgpstracker/fragments/MainFragment.kt
+++ app/src/main/java/com/galileosky/gsgpstracker/fragments/MainFragment.kt
@@ -10,6 +10,8 @@ import androidx.fragment.app.Fragment
 import com.galileosky.gsgpstracker.databinding.FragmentMainBinding
 import org.osmdroid.config.Configuration
 import org.osmdroid.library.BuildConfig
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
@@ -24,12 +26,37 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initOSM()
+    }
+
     private fun settingsOsm() {
         Configuration.getInstance().load(
             activity as AppCompatActivity,
             activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
         )
         Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
+    }
+
+    private fun initOSM() = with(binding){
+        map.controller.setZoom(18.0)
+        // Создается экземпляр класса GpsMyLocationProvider, который используется для получения данных о местоположении устройства с помощью GPS.
+        // Этот провайдер будет передавать данные о местоположении для использования на карте.
+        val mLocProvider = GpsMyLocationProvider(activity)
+        /// Создается объект MyLocationNewOverlay, который отвечает за отображение текущего местоположения пользователя на карте.
+        // Он использует mLocProvider для получения данных о местоположении и связывается с картой map.
+        val myLocOverLay = MyLocationNewOverlay(mLocProvider, map)
+        // Эта строка активирует отображение текущего местоположения пользователя на карте.
+        // Если GPS или другие источники местоположения включены и предоставляют данные, на карте будет отображаться маркер, указывающий текущее положение пользователя.
+        myLocOverLay.enableMyLocation()
+        myLocOverLay.enableFollowLocation()
+        myLocOverLay.runOnFirstFix {
+            map.overlays.clear()
+            map.overlays.add(myLocOverLay)
+        }
+        //map.controller.animateTo(GeoPoint(58.00171, 56.295304))
+        //58.00171° 56.295304°
     }
 
 

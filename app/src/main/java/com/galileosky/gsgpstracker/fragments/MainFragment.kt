a63e088a6a18402845266f1b466c03b9443d7c2b
gimasteevroman <best.roma@gmail.com>
2024-08-21 08:31:13 +0500

    Задача №10-1 1) Добавить функцию для сохранения настроек OpenStreetMap - settingsOsm() 2) Карта работает!

--- app/src/main/java/com/galileosky/gsgpstracker/fragments/MainFragment.kt
+++ app/src/main/java/com/galileosky/gsgpstracker/fragments/MainFragment.kt
@@ -1,11 +1,15 @@
 package com.galileosky.gsgpstracker.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.galileosky.gsgpstracker.databinding.FragmentMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
@@ -15,8 +19,17 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        settingsOsm()
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun settingsOsm() {
+        Configuration.getInstance().load(
+            activity as AppCompatActivity,
+            activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
+        )
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
     }
 
 

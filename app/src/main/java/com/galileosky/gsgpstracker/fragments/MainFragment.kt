f50cc5ec9d60fb297e2f06b3b30d41961eb24549
gimasteevroman <best.roma@gmail.com>
2024-08-22 18:10:26 +0500

    Задача №12 1) Делаю запрос пользователя на разрешение получения местоположения

--- app/src/main/java/com/galileosky/gsgpstracker/fragments/MainFragment.kt
+++ app/src/main/java/com/galileosky/gsgpstracker/fragments/MainFragment.kt
@@ -1,13 +1,20 @@
 package com.galileosky.gsgpstracker.fragments
 
+import android.Manifest
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.galileosky.gsgpstracker.databinding.FragmentMainBinding
+import com.galileosky.gsgpstracker.utils.checkPermission
+import com.galileosky.gsgpstracker.utils.showToast
 import org.osmdroid.config.Configuration
 import org.osmdroid.library.BuildConfig
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
@@ -15,7 +22,8 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
-
+    // переменная для разршений, работающая с массивом строк
+    private lateinit var pLauncher: ActivityResultLauncher<Array<String>>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,7 +36,8 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initOSM()
+        registerPermissions()
+        checkLocPermission()
     }
 
     private fun settingsOsm() {
@@ -57,6 +66,58 @@ class MainFragment : Fragment() {
         }
         //map.controller.animateTo(GeoPoint(58.00171, 56.295304))
         //58.00171° 56.295304°
+    }
+
+    // запуск лаунчера для окна разрешения
+    private fun registerPermissions(){
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()){
+                if (it[Manifest.permission.ACCESS_FINE_LOCATION] == true){ // приравниваем к true, чтобы исключить null
+                    initOSM()
+                } else {
+                    showToast("Вы не дали разрешения на использование местоположения")
+                }
+        }
+    }
+
+    // для разных версий андроид требуется различные разрешения. Для андроид 10 и ранее одно разрешение, а для старше 10 - 2
+    private fun checkLocPermission(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+            checkPermissonAfter10()
+        } else {
+            checkPermissonBefore10()
+        }
+    }
+
+    // проверяем разрешения при версии 10 и выше
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun checkPermissonAfter10() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            && checkPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        ) {
+            initOSM() // если есть оба разрешения, то все ОК
+        } else { // если нет, то спрашиваем оба разрешения
+            pLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                )
+            )
+        }
+    }
+
+    // проверяем разрешения до версии 10 и выше
+    private fun checkPermissonBefore10() {
+        if (checkPermission(Manifest.permission.ACCESS_FINE_LOCATION))
+        {
+            initOSM() // если есть разрешение, то все ОК
+        } else { // если нет, то спрашиваем разрешение
+            pLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            )
+        }
     }
 
 
--- app/src/main/java/com/galileosky/gsgpstracker/utils/Extensions.kt
+++ app/src/main/java/com/galileosky/gsgpstracker/utils/Extensions.kt
@@ -1,7 +1,9 @@
 package com.galileosky.gsgpstracker.utils
 
+import android.content.pm.PackageManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.galileosky.gsgpstracker.R
 
@@ -33,4 +35,12 @@ fun Fragment.showToast(s: String) {
 // функции для тостов в активити
 fun AppCompatActivity.showToast(s: String) {
     Toast.makeText(this,s, Toast.LENGTH_SHORT).show()
+}
+
+// Функция запроса разрешения
+fun Fragment.checkPermission(p: String): Boolean{
+    return when(PackageManager.PERMISSION_GRANTED){ //PERMISSION_GRANTED == 0 это значит что разрешение получено
+        ContextCompat.checkSelfPermission(activity as AppCompatActivity, p) -> true
+        else -> false
+    }
 }
